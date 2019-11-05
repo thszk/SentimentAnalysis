766c2cc004aff7073dbb9c46383f4c437192f1e0
thszk <thsuzuqui@gmail.com>
2019-11-05 08:36:06 -0400

    updating

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
+// libs
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const bodyparser = require("body-parser")
 const { exec } = require("child_process")
 const path = require("path")
 
+// preparing to create server
 const app = express()
-app.use(bodyparser.json() );     
 const port = process.env.PORT || 3333
-
+app.use(bodyparser.json())
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 
-
+// creating server
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
 })
 
-
+// routes
 app.get("/", (req, res) => {
   res.render("pages/home", {
     output: "",
@@ -34,7 +35,7 @@ app.get("/freeText", (req, res) => {
 app.get("/freeText/:input", (req, res) => {
   let input = JSON.stringify(req.params.input.replace(/%0D%0A/g,"\n")).replace(/\"/g,"")
   
-  exec(`echo "${input}" | ${path.resolve("./src/main.py")}`, (error, stdout) => {
+  exec(`echo "${input}" | ${path.resolve("./src/simple-analysis.py")}`, (error, stdout) => {
     let output
     error ? output = error : output = stdout
     res.render("pages/freeText", {
