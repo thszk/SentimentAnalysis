df804585d3e53db7009233a769f4f627fbb0ae39
thszk <thsuzuqui@gmail.com>
2019-11-12 16:00:07 -0400

    update training range

--- src/airline-training.py
+++ src/airline-training.py
@@ -24,7 +24,7 @@ with open('src/resources/Airline-Sentiment-2-w-AA.csv', 'r', encoding="ISO-8859-
 csvFile.close()
 
 # create training array
-for i in range(1300):
+for i in range(1150):
 	training.append(dataset[i])
 
 # training
