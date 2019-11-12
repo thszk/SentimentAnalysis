d9caec8b297eb894fdba50f2e4ef37ad07bf7f8e
thszk <thsuzuqui@gmail.com>
2019-11-12 10:53:46 -0400

    add training module

--- src/airline-analysis.py
+++ src/airline-analysis.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 
 # libs
-import csv
-import nltk
-from textblob.classifiers import NaiveBayesClassifier
+import pickle
 
-# vars
-dataset = []
-train = []
-
-# settungs
-nltk.download('punkt')
+# load  NB classifier object in text file
+with open('src/resources/nbclassifier', 'rb') as file:
+	classifier = pickle.load(file)
 
 # get inline parameter
 line = ""
@@ -19,26 +14,6 @@ while True:
     line = line + "\n" + input()
   except EOFError:
     break
-
-# read csv file
-with open('src/resources/Airline-Sentiment-2-w-AA.csv', 'r', encoding="ISO-8859-1") as csvFile:
-	reader = csv.reader(csvFile)
-	# append rows in dataset array
-	for row in reader:
-		dataset.append((row[14],row[5]))
-
-# close csv  file
-csvFile.close()
-
-# create training array
-for i in range(500):
-	train.append(dataset[i])
-
-# training
-# print("Treinando...")
-classifier = NaiveBayesClassifier(train)
-
-# print("Acuracia:", classifier.accuracy(train))
 
 # classifying
 print(classifier.classify(line))
---
+++ src/airline-training.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# libs
+import csv
+import nltk
+import pickle
+from textblob.classifiers import NaiveBayesClassifier
+
+# vars
+dataset = []
+training = []
+
+# settungs
+nltk.download('punkt')
+
+# read csv file
+with open('src/resources/Airline-Sentiment-2-w-AA.csv', 'r', encoding="ISO-8859-1") as csvFile:
+	reader = csv.reader(csvFile)
+	# append rows in dataset array
+	for row in reader:
+		dataset.append((row[14],row[5]))
+
+# close csv  file
+csvFile.close()
+
+# create training array
+for i in range(2000):
+	training.append(dataset[i])
+
+# training
+classifier = NaiveBayesClassifier(training)
+
+# save NB classifier object in text file
+with open('src/resources/nbclassifier', 'wb') as file:
+  pickle.dump(classifier, file)
+
+# print("Acuracia:", classifier.accuracy(training))
